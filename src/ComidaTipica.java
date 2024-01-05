0992ccedd5a401693df79df38718eebf9c78664b
jeseniaisabel231 <130105827+jeseniaisabel231@users.noreply.github.com>
2024-01-05 12:38:00 -0500

    seguir haciendo

--- src/ComidaTipica.java
+++ src/ComidaTipica.java
@@ -1,3 +1,15 @@
 public class ComidaTipica {
+    String nombre;
 
+    public ComidaTipica(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
